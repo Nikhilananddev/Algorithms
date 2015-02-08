86f9c6b6b2a116fe1ad44a60ddd82b69a76c8230
rainz <rainzforever@gmail.com>
2015-02-08 12:57:37 -0500

    Solved merge intervals

---
+++ src/com/rainz/MergeIntervals.java
@@ -0,0 +1,55 @@
+package com.rainz;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * Created by Yu on 2/8/2015.
+ */
+public class MergeIntervals {
+    static class Interval {
+        int start;
+        int end;
+        Interval() { start = 0; end = 0; }
+        Interval(int s, int e) { start = s; end = e; }
+    }
+
+    public static void test(String args[]) {
+        int[][] test = {{15,18},{1,3},{2,6},{8,10}};
+        List<Interval> input = new ArrayList<Interval>();
+        for (int[] pair: test)
+            input.add(new Interval(pair[0], pair[1]));
+        List<Interval> answer = merge(input);
+        for (Interval v: answer) {
+            System.out.printf("[%d,%d] ", v.start, v.end);
+        }
+        System.out.println();
+    }
+
+    public static List<Interval> merge(List<Interval> intervals) {
+        Collections.sort(intervals, new Comparator<Interval>() {
+            @Override
+            public int compare(Interval o1, Interval o2) {
+                return Integer.compare(o1.start, o2.start);
+            }
+        });
+        List<Interval> answer = new ArrayList<Interval>();
+        Interval curr = null;
+        for (Interval v: intervals) {
+            if (curr == null) {
+                curr = v;
+                answer.add(curr);
+                continue;
+            }
+            if (v.start <= curr.end) {
+                curr.end = Math.max(curr.end, v.end);
+                continue;
+            }
+            curr = v;
+            answer.add(curr);
+        }
+        return answer;
+    }
+}
