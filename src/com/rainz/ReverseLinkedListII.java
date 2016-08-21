5dcb39a5b221464e4e9d2b2dd7ecc13bf8e734de
Y Z <rainzforever@gmail.com>
2016-08-21 02:51:34 -0400

    Solved reverse linked list II

---
+++ src/com/rainz/ReverseLinkedListII.java
@@ -0,0 +1,52 @@
+package com.rainz;
+
+/**
+ * Created by Yu on 8/21/2016.
+ */
+public class ReverseLinkedListII {
+    public static void test(String args[]) {
+
+    }
+    public static class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) { val = x; }
+    }
+    public static ListNode reverseBetween(ListNode head, int m, int n) {
+        int idx = 0;
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode revHead = null; // the node before the list being reversed
+        ListNode node = dummy;
+        // Scan until we find the start of the section being reversed
+        while (node != null && revHead == null) {
+            if (idx == m - 1) {
+                revHead = node;
+            }
+            node = node.next;
+            ++idx;
+        }
+        if (revHead == null) {
+            // m is out of range
+            return head;
+        }
+        // Start reversing
+        ListNode firstRev = node;
+        ListNode prev = null;
+        while (node != null) {
+            ListNode nextNode = node.next;
+            node.next = prev;
+            if (idx == n) {
+                firstRev.next = nextNode;
+                revHead.next = node;
+                break;
+            }
+            prev = node;
+            node = nextNode;
+            ++idx;
+        }
+        // Note: this can't handle the case where n > length of the list.
+        return dummy.next;
+    }
+}
